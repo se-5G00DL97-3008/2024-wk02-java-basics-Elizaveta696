b26e4122292ab36283a4199414621304e08297bd
Elizaveta Bykova <148768133+Elizaveta696@users.noreply.github.com>
2024-01-11 22:29:42 +0200

    Update E02.java

--- e02/E02.java
+++ e02/E02.java
@@ -5,7 +5,7 @@ class E02 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give a number: ");
         int input_1 = scanner.nextInt();
-        System.out.println("Give another number: ");
+        System.out.println("Give a number: ");
         int input_2 = scanner.nextInt();
         System.out.println(String.format("%s + %s = %s", input_1, input_2, input_1 + input_2));
         System.out.println(String.format("%s * %s = %s", input_1, input_2, input_1 * input_2));
