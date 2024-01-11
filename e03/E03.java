37d43c205f449bf54109a8036fe7318e8c8b7d5e
Elizaveta Bykova <148768133+Elizaveta696@users.noreply.github.com>
2024-01-11 22:34:37 +0200

    Update E03.java

--- e03/E03.java
+++ e03/E03.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class E03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number:");
+        System.out.println("Enter a number: ");
         int num = scanner.nextInt();
         if (num > 0) {
             System.out.println("Positive number");
